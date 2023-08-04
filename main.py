881b49d5a793c77c43ffbd252506424af5d30a84
ilai <ilai.aloni@gmail.com>
2023-08-03 22:55:00 -0700

    Added try and except for input error

--- main.py
+++ main.py
@@ -9,23 +9,26 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+try:
+    if place:
+        filtered_data = get_data(place, days)
 
-if place:
-    filtered_data = get_data(place, days)
+        if option == "Temperature":
+            temperatures = [dict['main']["temp"] for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
 
-    if option == "Temperature":
-        temperatures = [dict['main']["temp"] for dict in filtered_data]
-        dates = [dict['dt_txt'] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
 
-    if option == "Sky":
-        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            print(sky_conditions)
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(image_paths)
+            st.image(image_paths, width=115)
+except KeyError:
 
-        sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
-        print(sky_conditions)
-        image_paths = [images[condition] for condition in sky_conditions]
-        print(image_paths)
-        st.image(image_paths, width=115)
+    st.write("The place you entered does not exist")
